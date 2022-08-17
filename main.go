0368b73c849628a66f8b11a475063d7d0bdef249
hud0shnik <c4nsero@mail.ru>
2022-08-17 21:32:22 +0300

    update main: add api part
    Добавил часть с апихой

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type api_response struct {
@@ -15,6 +19,13 @@ type api_response struct {
 type status struct {
 	Git string `json:"github_api"`
 	Osu string `json:"osu_api"`
+}
+
+func check_api() status {
+	return status{
+		Git: pingApi("https://osustatsapi.herokuapp.com/user/hud0shnik"),
+		Osu: pingApi("https://hud0shnikgitapi.herokuapp.com/user/hud0shnik"),
+	}
 }
 
 func pingApi(url string) string {
@@ -32,13 +43,15 @@ func pingApi(url string) string {
 	if response.Error == "" {
 		return "Ok"
 	}
+
 	return "Api error"
 
 }
 
-func main() {
+func track_api() {
 
 	for {
+
 		fmt.Println("| " + string(time.Now().Format("15:04")) + " check |")
 		fmt.Println("| osu:\t" + pingApi("https://osustatsapi.herokuapp.com/user/hud0shnik") + "    |")
 		fmt.Println("| git:\t" + pingApi("https://hud0shnikgitapi.herokuapp.com/user/hud0shnik") + "    |")
@@ -46,4 +59,30 @@ func main() {
 
 		time.Sleep(time.Minute * 5)
 	}
+}
+
+// Функция отправки информации о статусе пользователя
+func sendStatus(writer http.ResponseWriter, request *http.Request) {
+
+	// Заголовок, определяющий тип данных респонса
+	writer.Header().Set("Content-Type", "application/json")
+
+	// Обработка данных и вывод результата
+	json.NewEncoder(writer).Encode(check_api())
+}
+
+func main() {
+
+	go func() {
+		track_api()
+	}()
+
+	// Роутер
+	router := mux.NewRouter()
+	router.HandleFunc("/status", sendStatus).Methods("GET")
+	router.HandleFunc("/status/", sendStatus).Methods("GET")
+
+	// Запуск API
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+
 }
